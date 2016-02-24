83cf27fb31a7c261e7fc58f1e296f815bb4d3a4b
moonii <mhlee@crossent.com>
2016-02-24 18:12:25 +0900

    modified findbugs-exe.sh 7

--- pipe-task/findbugs-exe.sh
+++ pipe-task/findbugs-exe.sh
@@ -19,13 +19,13 @@ echo '..... cat $findbugs_report_file | grep -n $findbugs_err_word'
 cat $findbugs_report_file | grep -n "$findbugs_err_word"
 
 echo`..... cat $findbugs_report_file | grep -n $findbugs_err_word | awk -F $findbugs_err_word '{print 1}'`
-cat $findbugs_report_file | grep -n $findbugs_err_word | awk -F $findbugs_err_word '{print $1}'
+cat $findbugs_report_file | grep -n "$findbugs_err_word" | awk -F "$findbugs_err_word" '{print $1}'
 
 echo`..... cat $findbugs_report_file | grep -n $findbugs_err_word | awk -F $findbugs_err_word '{print 2}'`
-cat $findbugs_report_file | grep -n $findbugs_err_word | awk -F $findbugs_err_word '{print $2}'
+cat $findbugs_report_file | grep -n "$findbugs_err_word" | awk -F "$findbugs_err_word" '{print $2}'
 
 
-cnt $findbugs_report_file | grep -n "$findbugs_err_word" | awk -F $findbugs_err_word '{print $2}'`
+cnt=$findbugs_report_file | grep -n "$findbugs_err_word" | awk -F "$findbugs_err_word" '{print $2}'`
 
 echo "cnt =  $cnt"
 
