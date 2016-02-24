a7515250856d9ae401233aa06da17ec9ffe47911
moonii <mhlee@crossent.com>
2016-02-24 18:20:39 +0900

    modified findbugs-exe.sh 9

--- pipe-task/findbugs-exe.sh
+++ pipe-task/findbugs-exe.sh
@@ -4,7 +4,6 @@ findbugs_cmd="-Xmx512m -jar /findbugs-3.0.1/lib/findbugs.jar -textui "
 findbugs_report_file='findbugs_report.html'
 findbugs_err_word='Warnings generated: '
 
-
 # findbugs execute
 java $findbugs_cmd $1 | tee $findbugs_report_file
 
@@ -40,4 +39,3 @@ else
         echo "findbugs check success"
         exit 0
 fi
-
