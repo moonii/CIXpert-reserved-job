89244cea3b45e54046fb69dde68b0e4047f0863c
moonii <mhlee@crossent.com>
2016-02-24 17:27:58 +0900

    modified findbugs-exe.sh

--- pipe-task/findbugs-exe.sh
+++ pipe-task/findbugs-exe.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-findbugs_cmd="-Xmx512 -jar /findbugs-3.0.1/lib/findbugs.jar -textui "
+findbugs_cmd="-Xmx512m -jar /findbugs-3.0.1/lib/findbugs.jar -textui "
 findbugs_report_file='findbugs_report.html'
 findbugs_err_word='Warnings generated: '
 
@@ -8,11 +8,15 @@ findbugs_err_word='Warnings generated: '
 # findbugs execute
 java $findbugs_cmd $1 | tee $findbugs_report_file
 
-
 # keyword find & ...
 cnt=`cat $findbugs_report_file | grep -n $findbugs_err_word | awk -F $findbugs_err_word '{print $2}'`
 
 echo "cnt =  $cnt"
+
+if [ $cnt ]; then
+        exit 1
+fi
+
 if [ $cnt -gt 0 ]; then
 	echo "findbugs warnings $cnt detected"
         exit 1
