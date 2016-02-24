84a6f9d6ee82bd98e9996e85573ac67aac984bc3
moonii <mhlee@crossent.com>
2016-02-24 18:39:17 +0900

    modified findbugs-exe.sh 12

--- pipe-task/findbugs-exe.sh
+++ pipe-task/findbugs-exe.sh
@@ -11,19 +11,17 @@ java $findbugs_cmd $1 | tee $findbugs_report_file
 echo '..... ls -os '
 ls -os
 
-echo '..... cat $findbugs_report_file'
-cat $findbugs_report_file 
-
-echo '..... cat $findbugs_report_file | grep -n $findbugs_err_word'
-cat $findbugs_report_file | grep -n "$findbugs_err_word"
-
+line_cnt='cat $findbugs_report_file | wc -l'
 cnt=`cat $findbugs_report_file | grep -n "$findbugs_err_word" | awk -F "$findbugs_err_word" "{print $2}"`
 
-echo "cnt =  $cnt"
+echo "cnt =  $cnt| $findbugs_report_file line_cnt=$line_cnt"
 
 if [ $cnt ]; then
+	echo 'cnt is null'
         exit 1
 fi
+
+echo 'what is cnt'
 
 if [ $cnt -gt 0 ]; then
 	echo "findbugs warnings $cnt detected"
