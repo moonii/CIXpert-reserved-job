b4adb53ee3685c189111452319a0270fb454cfc7
moonii <mhlee@crossent.com>
2016-02-26 10:55:27 +0900

    pipe-task/findbugs-exe.sh

--- pipe-task/findbugs-exe.sh
+++ pipe-task/findbugs-exe.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sht $findbugs_report_file | grep -n "$findbugs_err_word" 
 
 findbugs_cmd="-Xmx512m -jar /findbugs-3.0.1/lib/findbugs.jar -textui "
 findbugs_report_file='findbugs_report.html'
@@ -14,16 +14,23 @@ ls -os
 #echo '..... cat $findbugs_report_file'
 #cat $findbugs_report_file 
 
+err_word_cnt=`grep -n "$findbugs_err_word" $findbugs_report_file | wc -l`
 bug_cnt=`cat $findbugs_report_file | grep -n "$findbugs_err_word" | awk -F "$findbugs_err_word" '{print $2}'`
 line_cnt=`cat $findbugs_report_file | wc -l`
 
-echo "bug_cnt =  $bug_cnt# ($findbugs_report_file)line_cnt=$line_cnt"
+echo "bug_cnt=$bug_cnt=($findbugs_report_file)line_cnt=$line_cnt"
 
-if [ $bug_cnt -gt 0 ]; then
-	echo "findbugs warnings $bug_cnt detected"
-        exit 1
+if [ $err_word_cnt -ne 0 ]; then
+
+	if [ $bug_cnt -gt 0 ]; then
+		echo "findbugs warnings $bug_cnt detected"
+        	exit 1
+	else
+	        echo "findbugs check success"
+        	exit 0
+	fi
+
 else
-        echo "findbugs check success"
-        exit 0
+	exit 0
+	
 fi
-
