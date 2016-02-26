3889ae48ca22c1414928ffd70975240f603d8cb8
moonii <mhlee@crossent.com>
2016-02-26 10:31:10 +0900

    findbugs-task/findbugs-exe.sh

--- findbugs-task/findbugs-exe.sh
+++ findbugs-task/findbugs-exe.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sht $findbugs_report_file | grep -n "$findbugs_err_word" 
 
 findbugs_cmd="-Xmx512m -jar /findbugs-3.0.1/lib/findbugs.jar -textui "
 findbugs_report_file='findbugs_report.html'
@@ -14,12 +14,13 @@ ls -os
 #echo '..... cat $findbugs_report_file'
 #cat $findbugs_report_file 
 
+has_err_word=`cat $findbugs_report_file | grep -n "$findbugs_err_word"`
 bug_cnt=`cat $findbugs_report_file | grep -n "$findbugs_err_word" | awk -F "$findbugs_err_word" '{print $2}'`
 line_cnt=`cat $findbugs_report_file | wc -l`
 
-echo "bug_cnt =  $bug_cnt# ($findbugs_report_file)line_cnt=$line_cnt"
+echo "bug_cnt=$bug_cnt=($findbugs_report_file)line_cnt=$line_cnt"
 
-if [ $bug_cnt ]; then
+if [ $has_err_word ]; then
 
 	if [ $bug_cnt -gt 0 ]; then
 		echo "findbugs warnings $bug_cnt detected"
