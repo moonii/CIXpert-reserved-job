7fcb9a4dd8ea16c800f9f158d8869a89521b1750
moonii <mhlee@crossent.com>
2016-03-08 17:13:45 +0900

    pmd-task/pmd-exe.sh no ruleset check

--- pmd-task/pmd-exe.sh
+++ pmd-task/pmd-exe.sh
@@ -6,9 +6,16 @@
 pmd_cmd="-Xmx512m -jar /pmd-bin-4.2.6/lib/pmd-4.2.6.jar $2/ xml $1/pmd-task/EgovInspectionRules_kor.xml"
 pmd_err_word='<violation'
 pmd_report_file='pmd_report.xml'
+pmd_no_ruleset='Ruleset not found'
 
 java $pmd_cmd | tee $pmd_report_file
 cnt="$(grep -c $pmd_err_word $pmd_report_file)"
+err_word_cnt=`grep -n "$pmd_no_ruleset" $pmd_report_file | wc -l`
+
+if [ $err_word_cnt -gt 0]; then
+	echo "$pmd_no_ruleset"
+	exit 1
+fi
 
 if [ $cnt -gt 0 ]; then
 	echo "pmd violation $cnt deteded"
