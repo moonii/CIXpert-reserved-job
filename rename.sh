0b3b29818f2415ea97b1349d272eb6412a57e429
moonii <mhlee@crossent.com>
2016-04-05 19:23:56 +0900

    rename.sh

--- rename.sh
+++ rename.sh
@@ -4,8 +4,8 @@ set -e
 
 echo "rename-release-exe.sh called"
 
-VERSION="$(cat version/number)"
-
+#VERSION="$(cat version/number)"
+VERSION="1.0.0.rc"
 echo "VERSION=$VERSION="
 
 cp pkg-out/PaasXpert-Portal-0.0.1.war renamed-out/PaasXpert-Portal-${VERSION}.war
