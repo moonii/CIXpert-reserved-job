0005a205c1d1a501b2319a3e053744d0104bb930
moonii <mhlee@crossent.com>
2016-04-18 20:36:52 +0900

    pmd-task/pmd-exe.sh echo

--- pmd-task/pmd-exe.sh
+++ pmd-task/pmd-exe.sh
@@ -7,7 +7,7 @@ pmd_cmd="-Xmx512m -jar /pmd-bin-4.2.6/lib/pmd-4.2.6.jar $2/ xml $RULE_PATH"
 pmd_err_word='<violation'
 pmd_report_file='pmd_report.xml'
 pmd_no_ruleset='Ruleset not found'
-
+echo "pmd_cmd=$pmd_cmd="
 java $pmd_cmd | tee $pmd_report_file
 cnt="$(grep -c $pmd_err_word $pmd_report_file)"
 err_word_cnt=`grep -n "$pmd_no_ruleset" $pmd_report_file | wc -l`
