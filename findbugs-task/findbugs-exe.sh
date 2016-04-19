9303c8243d410f21d2b7ea7d705afb5fa31e5533
moonii <mhlee@crossent.com>
2016-04-19 09:40:56 +0900

    added VERSION

--- findbugs-task/findbugs-exe.sh
+++ findbugs-task/findbugs-exe.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 
-findbugs_cmd="-Xmx512m -jar /findbugs-3.0.1/lib/findbugs.jar -textui "
+findbugs_cmd="-Xmx512m -jar /findbugs-$VERSION/lib/findbugs.jar -textui "
 findbugs_report_file='findbugs_report.html'
 findbugs_err_word='Warnings generated: '
 findbugs_err_not_found='File not found: '
--- pmd-task/pmd-exe.sh
+++ pmd-task/pmd-exe.sh
@@ -3,12 +3,13 @@
 # $1 : ci resource name
 # $2 : project resource name
 
-pmd_cmd="-Xmx512m -jar /pmd-bin-4.2.6/lib/pmd-4.2.6.jar $2/ xml $RULE_PATH"
+#pmd_cmd="-Xmx512m -jar /pmd-bin-4.2.6/lib/pmd-4.2.6.jar $2/ xml $RULE_PATH"
+pmd_cmd="-Xmx512m -jar /pmd-bin-$VERSION/lib/pmd-$VERSION.jar $2/ xml $RULE_PATH"
 pmd_err_word='<violation'
 pmd_report_file='pmd_report.xml'
 pmd_no_ruleset='Ruleset not found'
 
-echo "RULE_PATH=$RULE_PATH=pmd_cmd=$pmd_cmd="
+echo "RULE_PATH=$RULE_PATH=VERSION=$VERSION=pmd_cmd=$pmd_cmd="
 echo "env...."
 env
 
