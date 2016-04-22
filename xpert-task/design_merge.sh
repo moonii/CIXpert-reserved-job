99c90fd78ac31024208164a7cd6366bf0044d4f4
moonii <mhlee@crossent.com>
2016-04-22 16:22:44 +0900

    xpert-task/

--- xpert-task/design_merge.sh
+++ xpert-task/design_merge.sh
@@ -7,7 +7,11 @@ echo "design_merge.sh called"
 echo "ls -l source-git"
 ls -l source-git
 
+echo "ls -l source-git/paasxpert.portal.web"
+ls -l source-git/paasxpert.portal.web
+
 echo "ls -l design-git"
 ls -l design-git
 
-
+echo "ls -l design-git/PaaSXpertPortal"
+ls -l design-git/PaaSXpertPortal
