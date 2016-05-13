71e416e93be47dcbf1d933f7b4a86ea1d355f1e1
moonii <mhlee@crossent.com>
2016-05-13 16:01:05 +0900

    mvn-task/mvn-exe.sh modified

---
+++ mvn-task/install-protoc-apt.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+VERSION=2.6.1
+apt-get -y install build-essential
+wget https://github.com/google/protobuf/releases/download/v$VERSION/protobuf-$VERSION.tar.gz
+echo "1...............................PWD=$PWD and ls -l"
+ls -l
+tar xvfz protobuf-$VERSION.tar.gz
+echo "after tar"
+ls -l
+cd protobuf-$VERSION
+echo "2...............................PWD=$PWD and ls -l"
+ls -l
+./configure --prefix=/usr
+echo "3...............................configure"
+make
+make check
+sudo make install
+cd ..
+rm protobuf-$VERSION.tar.gz
+rm -rf protobuf-$VERSION
