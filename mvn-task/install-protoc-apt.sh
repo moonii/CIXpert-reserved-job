bba41794dc383752d6a556299319e2002b9ede2b
moonii <mhlee@crossent.com>
2016-05-13 17:03:48 +0900

    added apt-get update

--- mvn-task/install-protoc-apt.sh
+++ mvn-task/install-protoc-apt.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 #apt-get install make
-wget http://ftp.gnu.org/pub/gnu/make/make-3.81.tar.bz2
-tar xfj make-3.81.tar.bz2
-cd make-3.81
-./configure
-make
-sudo make install
-
-cd ..
+apt-get update
+apt-get -y install make
+which make
+#wget http://ftp.gnu.org/pub/gnu/make/make-3.81.tar.bz2
+#tar xfj make-3.81.tar.bz2
+#cd make-3.81
+#./configure
+#make
+#sudo make install
+#cd ..
 
 VERSION=2.6.1
 apt-get -y install build-essential
