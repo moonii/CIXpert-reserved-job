4d08af4795382d3ff245ef922b3a4150fbe40dcd
moonii <mhlee@crossent.com>
2016-05-31 17:01:51 +0900

    etc-task/cf-push.sh

---
+++ etc-task/cf-push.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#01.register Doamin 
+#echo "" > blank.file
+#echo "nameserver 52.21.208.105" > append.script 
+#sed -f append.script blank.file >> /etc/resolv.conf
+
+echo "nameserver 52.21.208.105" >> /etc/resolv.conf
+
+echo "	CF_API:      $CF_API
+	CF_USERNAME: $CF_USERNAME
+	CF_PASSWORD: $CF_PASSWORD
+        CF_ORG:      $CF_ORG
+        CF_SPACE:    $CF_SPACE
+	MANIFEST:    $MANIFEST
+	APP_PATH:    $APP_PATH"
+
+#02.cf push
+cf api $CF_API --skip-ssl-validation
+cf login -u "$CF_USERNAME" -p "$CF_PASSWORD"
+cf target -o "$CF_ORG" -s "$CF_SPACE"
+cf push -f "$MANIFEST" -p "$APP_PATH"
