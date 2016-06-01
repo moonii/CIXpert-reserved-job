1913a7a932d7dcd7023e733d6d2d4ae6fb58cd18
moonii <mhlee@crossent.com>
2016-06-01 14:50:23 +0900

    etc-task/cf-push.sh password

--- etc-task/cf-push.sh
+++ etc-task/cf-push.sh
@@ -15,15 +15,15 @@ sed '1 i\nameserver 52.21.208.105' temp.conf > /etc/resolv.conf
 echo "cat /etc/resolv.conf modified"
 cat /etc/resolv.conf
 
-echo "	CF_API:      $CF_API
-	CF_USERNAME: $CF_USERNAME
-	CF_PASSWORD: $CF_PASSWORD
-        CF_ORG:      $CF_ORG
-        CF_SPACE:    $CF_SPACE
-	MANIFEST:    $MANIFEST
-	APP_PATH:    $APP_PATH"
+echo "	CF_API:======$CF_API=
+	CF_USERNAME:=$CF_USERNAME=
+	CF_PASSWORD:=$CF_PASSWORD=
+        CF_ORG:======$CF_ORG=
+        CF_SPACE:====$CF_SPACE=
+	MANIFEST:====$MANIFEST=
+	APP_PATH:====$APP_PATH="
 
 #02.cf push
 cf api $CF_API --skip-ssl-validation
-cf login -u "$CF_USERNAME" -p "$CF_PASSWORD " -o "$CF_ORG" -s "$CF_SPACE"
+cf login -u "$CF_USERNAME" -p "$CF_PASSWORD" -o "$CF_ORG" -s "$CF_SPACE"
 cf push -f "$MANIFEST" -p "$APP_PATH"
